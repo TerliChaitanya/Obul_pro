899b915f46545fe616c87e0d776e0e6b1f2efac3
TerliChaitanya <chaitutherli123@gmail.com>
2026-02-19 16:18:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/obul_portfolio"
+mongo = PyMongo(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile_summary.html')
+
+@app.route('/skills')
+def skills():
+    return render_template('technical_skills.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/softskills')
+def softskills():
+    return render_template('softskills.html')
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html')
+
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
+
+@app.route('/submit_contact', methods=['POST'])
+def submit_contact():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+
+    mongo.db.contacts.insert_one({
+        "name": name,
+        "email": email,
+        "message": message
+    })
+
+    return redirect(url_for('contact_page'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,1 @@
+console.log('Multi-page Portfolio Loaded');
